18cbca66787e1f1b8b5f531005c65bf29fb99818
Samantha Thorne <150044477+13Thornes@users.noreply.github.com>
2024-06-11 19:16:05 -0230

    finished serving http

--- index.js
+++ index.js
@@ -1,56 +1,44 @@
 const http = require('http');
+const fs = require('fs');
 
 // serveText('Hello World');
-serveHtml('<html><body><b>Woot</b>Woot</body></html>')
+// serveHtml('<html><body><b>Woot</b>Woot</body></html>')
 // serveFile('index.html');
-// serveJson('{"name": "John", "age": 30, "city": "New York"}');
+serveJson('{"name": "John", "age": 30, "city": "New York"}');
 
-function serveText(theText) {
-    const server = http.createServer(function (req, res) {
-        console.log('The text was served')
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.write(theText);
-        res.write(', eat more lunch');
-        res.end();
-    });
 
-    server.listen(3000, () => {
-        console.log('Server is running on port 3000');
-    });
-}
+// function serveText(theText) {
+//     const server = http.createServer(function (req, res) {
+//         console.log('The text was served')
+//         res.writeHead(200, {'Content-Type': 'text/plain'});
+//         res.write(theText);
+//         res.write(', eat more lunch');
+//         res.end();
+//     });
 
-function serveHtml(TheHtml) {
-    const server = http.createServer(function (req,res) {
-        console.log('the html was served');
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(TheHtml);
-        res.end();
-    });
+//     server.listen(3000, () => {
+//         console.log('Server is running on port 3000');
+//     });
+// }
 
-    server.listen(3000, () => {
-        console.log('Server is running on port 3000');
-    });
+// function serveHtml(TheHtml) {
+//     const server = http.createServer(function (req,res) {
+//         console.log('the html was served');
+//         res.writeHead(200, {'Content-Type': 'text/html'});
+//         res.write(TheHtml);
+//         res.end();
+//     });
 
-    function serveFile(fileName){
-        const server = http.createServer(function (req, res) {
-          const theHtml = fs.readFileSync(fileName, 'utf8');
-          console.log('file was served.')
-          res.writeHead(200, {'Content-Type': 'text/html'});
-          res.write(theHtml);
-          res.end();
-        });
-      
-        server.listen(3000, () => {
-          console.log('Server is running on port 3000');
-        });
-      }
-      
-    //   function serveJson(theJson){
+//     server.listen(3000, () => {
+//         console.log('Server is running on port 3000');
+//     });
+
+    // function serveFile(fileName){
     //     const server = http.createServer(function (req, res) {
-    //       console.log(req.method);
-    //       console.log('json was served.')
-    //       res.writeHead(200, {'Content-Type': 'application/json'});
-    //       res.write(theJson);
+    //       const theHtml = fs.readFileSync(fileName, 'utf8');
+    //       console.log('file was served.')
+    //       res.writeHead(200, {'Content-Type': 'text/html'});
+    //       res.write(theHtml);
     //       res.end();
     //     });
       
@@ -58,7 +46,21 @@ function serveHtml(TheHtml) {
     //       console.log('Server is running on port 3000');
     //     });
     //   }
-}
+      
+      function serveJson(theJson){
+        const server = http.createServer(function (req, res) {
+          console.log(req.method);
+          console.log('json was served.')
+          res.writeHead(200, {'Content-Type': 'application/json'});
+          res.write(theJson);
+          res.end();
+        });
+      
+        server.listen(3000, () => {
+          console.log('Server is running on port 3000');
+        });
+      }
+// }
 
 
 
